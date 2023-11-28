702c758442c4f71fa59845e16b79959db0a35f43
vedmandal <151810723+vedmandal@users.noreply.github.com>
2023-11-28 18:35:58 +0530

    Updated

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ import cors from "cors"
 import CategoryRoute from "./Routes/CategoryRoute.js"
 import ProductRoute from "./Routes/ProductRoute.js"
 import path from "path"
+import { fileURLToPath } from "url"
 
 
 
 const app=express();
 
 dotenv.config();
+const __filename=fileURLToPath(import.meta.url)
+const __dirname=path.dirname(__filename);
 
 
 app.use(cors())
