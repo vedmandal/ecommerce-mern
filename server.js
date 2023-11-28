c47e7faa0f95a9298757dc5eaf6b0c5ecc58c906
Vedmandal <mandalved643@gmail.com>
2023-11-28 18:12:09 +0530

    final

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import AuthRoute from "./Routes/AuthRoutes.js"
 import cors from "cors"
 import CategoryRoute from "./Routes/CategoryRoute.js"
 import ProductRoute from "./Routes/ProductRoute.js"
+import path from "path"
+
 
 
 const app=express();
@@ -14,12 +16,15 @@ dotenv.config();
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname,'./client/build')))
 
 
 app.use("/api/v1/auth",AuthRoute);
 app.use("/api/v1/category",CategoryRoute);
 app.use("/api/v1/product",ProductRoute);
-
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
 connectdatabase();
 
 
